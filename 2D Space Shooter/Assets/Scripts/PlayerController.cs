806e812c21d5f01644ce22ca003c1863c63711b1
Yuvraj2705 <yg27052002@gmail.com>
2022-02-10 09:55:19 +0530

    Player Health Added

--- 2D Space Shooter/Assets/Scripts/PlayerController.cs
+++ 2D Space Shooter/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     [Header("Player Settings")]
     [SerializeField] float xPadding;
     [SerializeField] float speed;
+    public int health = 100;
  
     private float xMax, xMin;
 
---
+++ 2D Space Shooter/Assets/Scripts/SpaceShipHealth.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SpaceShipHealth : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+    [SerializeField] float maxHealth = 100;
+    [SerializeField] Image healthBar;
+    [SerializeField] CircleCollider2D collider2D;
+    [SerializeField] ParticleSystem explosion;
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        healthBar.fillAmount = health/maxHealth;
+    }
+
+    IEnumerator OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "EB")
+        {
+            health -= 1;
+            if(health <= 0)
+            {
+                var Instance = Instantiate(explosion,transform.position,Quaternion.identity);
+                Destroy(Instance.gameObject,1);
+                Destroy(gameObject);
+                yield return NextScene();
+            }
+            collider2D.enabled = false;
+            anim.SetBool("hurt",true);
+            yield return new WaitForSeconds(4f);
+            collider2D.enabled = true;
+            anim.SetBool("hurt",false);
+        }
+    }
+
+    IEnumerator NextScene()
+    {
+        Debug.Log("next");
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("Menu");
+    }
+}
